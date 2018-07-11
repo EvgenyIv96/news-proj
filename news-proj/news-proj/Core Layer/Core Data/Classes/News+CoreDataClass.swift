360d7504fc48b4c412a840ee0ed7d58b47b45cc3
Евгений Иванов <eivanov@baccasoft.ru>
2018-07-11 23:45:21 +0300

    Добавлен наследник для сущности News.

---
+++ news-proj/news-proj/Core Layer/Core Data/Classes/News+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  News+CoreDataClass.swift
+//  news-proj
+//
+//  Created by Евгений Иванов on 11.07.2018.
+//  Copyright © 2018 Евгений Иванов. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(News)
+public class News: NSManagedObject {
+
+}
---
+++ news-proj/news-proj/Core Layer/Core Data/Classes/News+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  News+CoreDataProperties.swift
+//  news-proj
+//
+//  Created by Евгений Иванов on 11.07.2018.
+//  Copyright © 2018 Евгений Иванов. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension News {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<News> {
+        return NSFetchRequest<News>(entityName: "News")
+    }
+
+    @NSManaged public var slug: String?
+    @NSManaged public var title: NSObject?
+    @NSManaged public var text: String?
+    @NSManaged public var viewsCount: Int16
+    @NSManaged public var creationDate: NSDate?
+
+}
