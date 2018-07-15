a7fd0fded2630dc4f14b49ce84832a88846d17ee
Евгений Иванов <eivanov@baccasoft.ru>
2018-07-15 23:04:50 +0300

    Интерфейс для сервиса детальной информации новости.

--- news-proj/news-proj/Business Logic Layer/NewsDetailService/NewsDetailServiceInput.swift
+++ news-proj/news-proj/Business Logic Layer/NewsDetailService/NewsDetailServiceInput.swift
@@ -7,7 +7,21 @@
 //
 
 import Foundation
+import CoreData
 
 protocol NewsDetailServiceInput: class {
+    
+    /// Method is used to set up service before usage. WARNING: Expected permanent managed object id!
+    ///
+    /// - Parameter newsObjectID: Permanent managed object id for news object.
+    func setup(with newsObjectID: NSManagedObjectID)
+    
+    /// Method is used to obtain plain news object.
+    ///
+    /// - Returns: Plain news object
+    func obtainNewsPlainObject() -> NewsPlainObject
+    
+    /// Method is used to update news data from server.
+    func reloadNewsData()
     
 }
