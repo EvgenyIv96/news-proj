3dc1f52c657062fbd353d226077524b6202ce193
Евгений Иванов <eivanov@baccasoft.ru>
2018-07-15 20:33:03 +0300

    Добавлен сториборд.

---
+++ news-proj/news-proj/Presentation Layer/NewsDetail/Storyboard/NewsDetailStoryboardConstants.swift
@@ -0,0 +1,12 @@
+//
+//  NewsDetailStoryboardConstants.swift
+//  news-proj
+//
+//  Created by Евгений Иванов on 15.07.2018.
+//  Copyright © 2018 Евгений Иванов. All rights reserved.
+//
+
+import Foundation
+
+let NewsDetailStoryboardName = "NewsDetailStoryboard"
+
